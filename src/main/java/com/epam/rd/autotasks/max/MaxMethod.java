a0d6b335ebd4961faaa7115e1c2093decf380475
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-03-01 16:02:19 +0100

    MaxMethod_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -2,6 +2,15 @@ package com.epam.rd.autotasks.max;
 
 public class MaxMethod {
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int max = values[0];
+        for (int x=0;x<values.length;x++) {
+            if (max < values[x]) {
+                max = values[x];
+            }
+        }
+        System.out.println(max);
+        return max;
     }
 }
+
+
